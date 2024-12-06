a76c37ffd07ecac20d35253ad8e36426ec30f6ae
Antonius Sakti Wiradinata <wira@xashlez.com>
2024-12-07 02:49:40 +0700

    [UPDATE] add comment

--- validators.py
+++ validators.py
@@ -4,5 +4,5 @@ def is_valid_news_url(url):
     # Cek apakah URL valid
     if not validators.url(url):
         return False
-    # Cek apakah URL berasal dari CNN Indonesia atau CNBC Indonesia
+    # Cek URL berasal dari CNN Indonesia atau CNBC Indonesia
     return "cnnindonesia.com" in url or "cnbcindonesia.com" in url
